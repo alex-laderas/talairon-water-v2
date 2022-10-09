0228ecd38712a10686b4fa916aa2f028d771db1e
alex-laderas <alexisladeras@gmail.com>
2022-10-09 14:51:29 +0800

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Login from '../src/pages/login'
 import Register from '../src/pages/register'
 import User from './pages/user';
 import Admin from './pages/admin'
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router"
 import './App.css';
 
 function App() {
